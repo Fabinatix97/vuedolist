7e15351731b2cf0af28a78c0d238a96668c1d514
Fabinatix97 <fabian@fabi-online.de>
2025-01-03 17:39:33 +0100

    Add theme support with CSS variables for light and dark modes in main.css and update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: "var(--body)",
+        border: "var(--border)",
+        info: "var(--info)",
+        main: "var(--main)",
+        primary: "var(--primary)",
+        primaryhover: "var(--primaryhover)",
+        secondary: "var(--secondary)",
+        secondaryhover: "var(--secondaryhover)",
+        text: "var(--text)",
+      },
+    },
   },
   plugins: [],
 }
-
