51e376aba924a3a1fdd2ddd552a134d481ce6056
Fabinatix97 <fabian@fabi-online.de>
2025-01-05 13:09:45 +0100

    Refactor App.vue and router to update theme handling and rename About route to List

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { Icon } from '@iconify/vue'
-import { ref, watch, computed } from 'vue'
+import { ref } from 'vue'
 
 const theme = ref(
   localStorage.getItem('theme') ||
     (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'),
 )
 
-const themeIconName = computed(() => {
-  return theme.value === 'light' ? 'material-symbols:nightlight' : 'material-symbols:sunny'
-})
-
 function applyTheme() {
-  //console.log(`Applying theme: ${theme.value}`);
   document.documentElement.setAttribute('data-theme', theme.value)
 }
 
-function toggleTheme() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-  localStorage.setItem('theme', theme.value)
-}
-
 applyTheme()
-watch(theme, applyTheme)
 </script>
 
 <template>
@@ -31,16 +20,13 @@ watch(theme, applyTheme)
     <RouterView class="border-2 border-red-500" />
     <nav class="flex justify-center gap-4 border-2 border-primary">
       <RouterLink to="/"><Icon icon="material-symbols:home" style="font-size: 2.5em" /></RouterLink>
-      <RouterLink to="/about"
+      <RouterLink to="/list"
         ><Icon icon="material-symbols:menu-rounded" style="font-size: 2.5em"
       /></RouterLink>
       <RouterLink to="/settings"
         ><Icon icon="material-symbols:settings-rounded" style="font-size: 2.5em"
       /></RouterLink>
     </nav>
-  </div>
-  <div class="mt-10 justify-self-center">
-    <Icon :icon="themeIconName" style="font-size: 2.5em" @click="toggleTheme()" />
   </div>
 </template>
 
